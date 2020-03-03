3165e4e758727a437968e4c467a631898bb3c2c9
Somya <somya@Somyas-MacBook-Pro.local>
2020-03-02 23:18:45 -0800

    Updated routes

--- handshake/backend/index.js
+++ handshake/backend/index.js
@@ -5,12 +5,16 @@
   const signup = require("./routes/signup");
   const profile = require("./routes/profile");
   const update = require("./routes/update");
+  const companyProfile = require("./routes/companyProfile");
+  const events = require("./routes/events");
   
   
   app.use("/login", login);
   app.use("/signup", signup);
   app.use("/profile", profile);
   app.use("/update", update);
+  app.use("/companyProfile", companyProfile);
+  app.use("/events", events);
   
   app.listen(3001, () => {
     console.log(`Server listening on port 3001`);
