6d33a17c271dffae1a5d383da119c4736d103329
Somya <somya@Somyas-MacBook-Pro.local>
2020-03-06 21:20:56 -0800

    Applications tab functionality added

--- handshake/frontend/src/components/StudentTabs/Applications.jsx
+++ handshake/frontend/src/components/StudentTabs/Applications.jsx
@@ -3,21 +3,95 @@ import '../../App.css';
 import axios from 'axios';
 import {Redirect} from 'react-router';
 import {connect} from 'react-redux';
-import { Modal, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import Modal from 'react-modal';
 
 class Applications extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            successUpdate: false,        
+            applications:[],
+            applicationstatus:'all'
+                
         };
+        this.handlePending = this.handlePending.bind(this);
+        this.handleReviewed = this.handleReviewed.bind(this);
+        this.handleDeclined = this.handleDeclined.bind(this);
+        this.handleAll = this.handleAll.bind(this);
     }
     componentDidMount(){
-        
+        this.viewApplication();
     }
-   
+    handleStatusChange=(e)=>{
+
+        this.setState({
+            applicationstatus: e.target.value
+        })
+        console.log(this.state.applicationstatus)
+        this.viewApplication();
+    }
+    handlePending(){
+        this.state.applicationstatus='pending'
+        this.setState({
+            applicationstatus:this.state.applicationstatus
+            
+        })
+          this.viewApplication();
+    }
+    handleReviewed(){
+        this.state.applicationstatus='reviewed'
+        this.setState({
+            applicationstatus:this.state.applicationstatus
+            
+        })
+          this.viewApplication();
+    }
+    handleDeclined(){
+        this.state.applicationstatus='declined'
+        this.setState({
+            applicationstatus:this.state.applicationstatus
+            
+        })
+         this.viewApplication();
+    }
+    handleAll(){
+        this.state.applicationstatus='all'
+        this.setState({
+            applicationstatus:this.state.applicationstatus
+            
+        })
+          this.viewApplication();
+    }
+
+
+    viewApplication=()=>
+    {
+        
+        let data = {
+            userType:localStorage.getItem("user_type"),
+            studentid:localStorage.getItem("user_id"),
+            status:this.state.applicationstatus
+        }
+        console.log(JSON.stringify(data))
+        axios.post('http://localhost:3001/jobs/getJobsApplied',data)
+        .then(response => {
+            console.log("Status Code : ",response.status);
+            if(response.status === 200){
+                let applications=response.data;
+                console.log(JSON.stringify(applications))
+                this.setState({
+                    applications   
+                });
+            }
+        })
+        .catch(err => { 
+            this.setState({errorMessage:"Job could not be viewed"});
+        });
+
+
+    }
     render() {
-        let redirectVar;
+        let redirectVar;let applicationlist;
         if (!localStorage.getItem("token")) {
             redirectVar = <Redirect to="/login" />;
         }
@@ -25,31 +99,38 @@ class Applications extends Component {
         return (
             <div>
             {redirectVar}
-            
-            
-    <div class="row">    
-        <div class="col-xs-8 col-xs-offset-2">
-		    <div class="input-group">
-                <div class="input-group-btn search-panel">
-                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                    	<span id="search_concept">Filter by</span> <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu" role="menu">
-                      <li><a href="#contains">Student Name</a></li>
-                      <li><a href="#its_equal">College Name</a></li>
-                      <li><a href="#greather_than">Skills</a></li>
-                    </ul>
-                </div>
-                <input type="hidden" name="search_param" value="all" id="search_param"/>         
-                <input type="text" class="form-control" name="x" placeholder="Search term..."/>
-                <span class="input-group-btn">
-                    <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search"></span></button>
-                </span>
-            </div>
+            <div className="container">
+        <div className="panel panel-default p50 uth-panel">
+        <table className="table table-hover">
+            <thead>
+                <tr>
+                    <th>Job Title</th>
+                    <th>Application Date</th>
+                    <th> <div class="dropdown">
+    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Status
+    <span class="caret"></span></button>
+    <ul class="dropdown-menu">
+        <li><a onClick={() => this.handleAll()}>All</a></li>
+      <li><a onClick={() => this.handlePending()}>Pending</a></li>
+      <li><a onClick={() => this.handleReviewed()}>Reviewed</a></li>
+      <li><a onClick={() => this.handleDeclined()}>Declined</a></li>
+    </ul>
+  </div></th>
+                    
+                </tr>
+            </thead>
+            <tbody>
+            {this.state.applications.map(application =>
+                <tr>
+                <td>{application.jobtitle} </td>
+            <td>{application.applicationdate}</td>
+            <td>{application.status}</td>
+                </tr>
+            )}
+             </tbody>
+        </table>
         </div>
-	</div>
-
-           
+        </div>
             </div>
            
             
--- handshake/frontend/src/components/StudentTabs/Students.jsx
+++ handshake/frontend/src/components/StudentTabs/Students.jsx
@@ -16,45 +16,89 @@ class Students extends Component {
         
     }
    
-    render() {
-        let redirectVar;
-        if (!localStorage.getItem("token")) {
-            redirectVar = <Redirect to="/login" />;
-        }
-       
-        return (
-            <div>
-            {redirectVar}
-            
-            
-    <div class="row">    
-        <div class="col-xs-8 col-xs-offset-2">
-		    <div class="input-group">
-                <div class="input-group-btn search-panel">
-                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                    	<span id="search_concept">Filter by</span> <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu" role="menu">
-                      <li><a href="#contains">Student Name</a></li>
-                      <li><a href="#its_equal">College Name</a></li>
-                      <li><a href="#greather_than">Skills</a></li>
-                    </ul>
-                </div>
-                <input type="hidden" name="search_param" value="all" id="search_param"/>         
-                <input type="text" class="form-control" name="x" placeholder="Search term..."/>
-                <span class="input-group-btn">
-                    <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search"></span></button>
-                </span>
+    render() 
+    { let redirectVar;let studentlist;
+    if (!localStorage.getItem("token")) {
+        redirectVar = <Redirect to="/login" />;
+    }
+    studentlist =( <div className="panel panel-default p50 uth-panel">
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Student Name</th>
+                        <th>College Name</th>
+                        <th>Major</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {this.state.students.map(student =>
+                    <tr key={student.EventId}>
+                    <td>{student.StudentName}</td>
+                    <td>{student.CollegeName}</td>
+                    <td>{student.Major}</td>
+                    <td><a onClick={() => this.openStudent(student)}>View Student Details</a></td>
+                    </tr>
+                )}
+                 </tbody>
+            </table>
+            </div>)
+   
+    return (
+        <div>
+        {redirectVar}
+        
+        <div className="container">     
+                    <div className="main-div">
+                        <div className="panel">
+                            <h2>Event Search</h2>
+                              
+<div class="row">    
+    <div class="col-xs-8 col-xs-offset-2">
+        <div class="input-group">
+            <div class="input-group-btn search-panel">
             </div>
+            <input type="text" class="form-control" name="search" placeholder="Event Title" onChange={this.eventCriteria}/>
+            <span class="input-group-btn">
+                <button class="btn btn-default" type="button" onClick={this.searchEvent}><span class="glyphicon glyphicon-search"></span></button>
+            </span>
         </div>
-	</div>
+    </div>
+</div>
 
-           
-            </div>
-           
-            
-    )
-  }
+</div>
+</div>
+{studentlist}
+
+<Modal
+                        isOpen={this.state.openevent}
+                        onRequestClose={this.closeModal}
+                         contentLabel="Example Modal" >
+                             <div>             
+                        <div class="container">
+                        <div class="panel panel-default">
+                        <div class="panel-heading">Event Details </div>
+                        <div class="panel-heading">Event Name: {this.state.eventname}</div>
+                        <div class="panel-body">Description: {this.state.description}</div>
+                        <div class="panel-body">Time: {this.state.time}</div>
+                        <div class="panel-body">Date: {this.state.date}</div>
+                        <div class="panel-body">Eligibility: {this.state.eligibility}</div>
+                        <div class="panel-body">Location: {this.state.location}{this.state.city}</div>
+                        </div>
+                        </div>
+                    </div>
+                         
+                     <center>
+                            <Button variant="primary" onClick={this.closeModal}>
+                                <b>Close</b>
+                            </Button>
+                        </center>
+                    </Modal>
+</div>
+</div>
+       
+        
+)
+}
 }
 
 
