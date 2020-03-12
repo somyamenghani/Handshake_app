99a548c63fa4f882a4e3ee4d12ae1b7bdf746f3a
Somya <somya@Somyas-MacBook-Pro.local>
2020-03-12 14:20:39 -0700

    Linting fix

--- handshake/frontend/src/common/action.js
+++ handshake/frontend/src/common/action.js
@@ -1,59 +1,60 @@
 
 import axios from 'axios';
+
 export const USER_PROFILE_UPDATE = 'profile_update';
 
-export function profileUpdate(values,callback) {
-    console.log('inside  user profile update action creator');
-    console.info('profile_update_values',values);
-    //const payload_response_data;
-    const request = axios.post('http://localhost:3001/update',values,{withCredentials:true});
+export function profileUpdate(values, callback) {
+  console.log('inside  user profile update action creator');
+  console.info('profile_update_values', values);
+  // const payload_response_data;
+  const request = axios.post('http://localhost:3001/update', values, { withCredentials: true });
 
-    return (dispatch) => {
-        request.then(
-            ({data}) => {
-                console.log("Inside the profile update dispatcher function");
-                console.log(data);
-                callback(data);
-                if (data.success) {
-                    dispatch({
-                        type: USER_PROFILE_UPDATE,
-                        payload: data
-                    });
-                }
-            }
-        )};
+  return (dispatch) => {
+    request.then(
+      ({ data }) => {
+        console.log('Inside the profile update dispatcher function');
+        console.log(data);
+        callback(data);
+        if (data.success) {
+          dispatch({
+            type: USER_PROFILE_UPDATE,
+            payload: data,
+          });
+        }
+      },
+    );
+  };
 }
 
-export function uploadFile(payload,callback) {
-    console.log("Inside the upload functionality");
-    const request = fetch('http://localhost:3001/files/upload', {
-        method: 'POST',
-        body: payload,
-        credentials: 'include',
-        headers: {
-            'Accept': 'application/json, application/xml, text/plain, text/html, *.*'
-        },
+export function uploadFile(payload, callback) {
+  console.log('Inside the upload functionality');
+  const request = fetch('http://localhost:3001/files/upload', {
+    method: 'POST',
+    body: payload,
+    credentials: 'include',
+    headers: {
+      Accept: 'application/json, application/xml, text/plain, text/html, *.*',
+    },
+  });
+  return (dispatch) => {
+    request.then((res) => {
+      console.info('response', res);
+      res.json().then((data) => {
+        console.log(data.filename);
+        if (data.filename !== undefined) {
+          callback(data.filename);
+        } else {
+          callback(false);
+        }
+      });
+    }).catch((error) => {
+      console.log('There is an error during Uploading of File');
+      return error;
     });
-    return (dispatch) => {
-        request.then(res => {
-            console.info('response',res);
-            res.json().then( data => {
-                console.log(data.filename);
-                if (data.filename !== undefined) {
-                    callback(data.filename);
-                }
-                else {
-                    callback(false);
-                }
-            }
-            )}).catch(error => {
-            console.log("There is an error during Uploading of File");
-            return error;
-        });
-        /*
+    /*
                 return {
                     type: CREATE_USER,
                     payload: request
-                };*/
-    };
+                }; */
+  };
 }
--- handshake/frontend/src/store/reducer.js
+++ handshake/frontend/src/store/reducer.js
@@ -1,56 +1,47 @@
 const initialState = {
-userId : 0,
-userName : "",
-userEmail :"",
-userType : 1
-}
+  userId: 0,
+  userName: '',
+  userEmail: '',
+  userType: 1,
+};
 
-const reducer =(state=initialState,action)=>{
-    if(action.type ==='LOADUSER')
-    {
-        
-        return{
-            userId : localStorage.getItem("user_id"),
-            userName : localStorage.getItem("user_name"),
-            userEmail:localStorage.getItem("email_id"),
-            userType:localStorage.getItem("user_type")            
-        }
-    }
-    if(action.type ==='UNLOADUSER')
-    {
-        return state;   
-    }
-    if(action.type ==='USERDETAILS')
-    {
-        
-        return{
-            userId : localStorage.getItem("user_id"),
-            userName : localStorage.getItem("user_name"),
-            userEmail:localStorage.getItem("email_id"),
-            userType:localStorage.getItem("user_type") ,
-            user:localStorage.getItem("user_profile")         
-        }
-    }
-    if(action.type ==='COMPANYDETAILS')
-    {
-        
-        return{
-            userId : localStorage.getItem("user_id"),
-            userName : localStorage.getItem("user_name"),
-            userEmail:localStorage.getItem("email_id"),
-            userType:localStorage.getItem("user_type") ,
-            user:localStorage.getItem("user_profile")         
-        }
-    }
-    if(action.type ==='SIGNUP')
-    {
-        
-        return{
-            signupFlag : "success"        
-        }
-    }
-    
+const reducer = (state = initialState, action) => {
+  if (action.type === 'LOADUSER') {
+    return {
+      userId: localStorage.getItem('user_id'),
+      userName: localStorage.getItem('user_name'),
+      userEmail: localStorage.getItem('email_id'),
+      userType: localStorage.getItem('user_type'),
+    };
+  }
+  if (action.type === 'UNLOADUSER') {
     return state;
-}
+  }
+  if (action.type === 'USERDETAILS') {
+    return {
+      userId: localStorage.getItem('user_id'),
+      userName: localStorage.getItem('user_name'),
+      userEmail: localStorage.getItem('email_id'),
+      userType: localStorage.getItem('user_type'),
+      user: localStorage.getItem('user_profile'),
+    };
+  }
+  if (action.type === 'COMPANYDETAILS') {
+    return {
+      userId: localStorage.getItem('user_id'),
+      userName: localStorage.getItem('user_name'),
+      userEmail: localStorage.getItem('email_id'),
+      userType: localStorage.getItem('user_type'),
+      user: localStorage.getItem('user_profile'),
+    };
+  }
+  if (action.type === 'SIGNUP') {
+    return {
+      signupFlag: 'success',
+    };
+  }
+
+  return state;
+};
 
 export default reducer;
