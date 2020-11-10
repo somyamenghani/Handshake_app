2590094456f4e9af26f325843497560def23ab8a
somya menghani <somyamenghani.3011@gmail.com>
2020-11-10 14:24:12 -0800

    Update config.js

--- handshake/backend/config/config.js
+++ handshake/backend/config/config.js
@@ -1,11 +1,11 @@
 "use strict";
 const config = {
-   secret: "cmpe273_kafka_passport_mongo",
-   awsBucket: "handshakepublic",
-   awsSecretAccessKey: 'icJMjnMYF6RYztOLMesEdrQQCbJHUrzWlLLR3MPZ',
-   awsAccessKey:'ASIARRWXJ7G4Z3W2MBV3',
+   secret: "",
+   awsBucket: "",
+   awsSecretAccessKey: '',
+   awsAccessKey:'',
    region: 'us-east-1',
-   awsSessionToken:'FwoGZXIvYXdzEHMaDBAyf32AuplQcX+JSSLGARj1sjfvUdHnU9DWgFrKTku0rDH/RdmAtVM6ZBRL2mpG8iyWt77WQB7+VhIU21clOeG/iXYT2X/fedZE6fLEF9n46/siKSd+6KyKPDZU3TKSoiXOoRfqVsokpRncSMshJNjLa8OhiHalVCkE6eqwZhDYpmmDDdyigcKtWnAE2rUN2V77chNlmAmkw+xZgzjRsnlxFv8D4Vw0eijP5Yjv+a7NT5lwFdjJhbvMupa18O/ELUoI8beG6ezCBT2XBLNcdryO+acjPSj2jK/zBTItoTCo3vB1yt2whU48QEbxu7DM+wGfhy5oYlUMuA9QCyGRcEI0ATvNd6TnXbJE',
+   awsSessionToken:'',
    awsPermission: "public-read"
 };
 
